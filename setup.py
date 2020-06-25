adbf8776135c40a7e93c33d36c2a7f26be599115
Michael Kopp <kopp.michael@yahoo.de>
2020-06-25 15:42:38 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+
+from typing import List
+
+from setuptools import find_packages, setup
+
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="calliope-mini-stubs",
+    version="0.0.1",
+    author="kopp",
+    author_email="kopp.michael@yahoo.de",
+    description="Provide stubs with documentation for the MicroPython running on Calliope Mini",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kopp/calliope-mini-stubs",
+    packages=["calliope_mini", "radio", "utime"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
