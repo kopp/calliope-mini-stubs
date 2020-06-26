f8d30ee483a1da3dfe29c90fda670e5736d02b0a
Michael Kopp <kopp.michael@yahoo.de>
2020-06-26 23:48:00 +0200

    shorter description on PyPi (images are not properly displayed there)

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
 from setuptools import setup
 
 
-with open("Readme.md", "r") as fh:
-    long_description = fh.read()
+GITHUB_URL = "https://github.com/kopp/calliope-mini-stubs"
 
 
 setup(
     name="calliope-mini-stubs",
-    version="0.0.5",
+    version="0.0.6",
     author="kopp",
     description="Provide stubs with documentation for the MicroPython running on Calliope Mini",
-    long_description=long_description,
+    long_description="""# CalliopeMini Stubs
+
+Use this library to develop your python programs for the
+[Calliope Mini](https://calliope.cc/)
+using a state-of-the-art python IDE such as
+[Visual Studio Code](https://code.visualstudio.com/)
+on your computer.
+
+This library only helps by providing documentation, type annotations and
+function signatures.
+It does not contain a simulator/emulator and it does not allow to transfer the
+developed code to the Calliope Mini.
+
+
+
+Please see
+[the repository on GitHub]({})
+for how a simple step-by-step instruction how to use it.
+    """.format(GITHUB_URL),
     long_description_content_type="text/markdown",
-    url="https://github.com/kopp/calliope-mini-stubs",
+    url=GITHUB_URL,
     packages=["calliope_mini", "radio", "utime"],
     classifiers=[
         "Programming Language :: Python :: 3",
