020bcb3967cbd9438b38a5e8e5b5b500af862225
Michael Kopp <kopp.michael@yahoo.de>
2020-09-09 21:34:27 +0200

    address further flake8 findings

--- calliope_mini/__init__.py
+++ calliope_mini/__init__.py
@@ -34,8 +34,7 @@ from .i2c import MicroBitI2C
 from .image import MicroBitImage
 from .led import CalliopeLED
 from .magnetometer import CalliopeMagnetometer
-from .pin import MicroBitAnalogDigitalPin, MicroBitDigitalPin, MicroBitTouchPin
-from .pin_instances import (
+from .pin_instances import (  # noqa: I101
     pin0, pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8,
     pin9, pin10, pin11, pin12, pin13, pin14, pin15, pin16,
     pin17, pin18, pin19, pin20, pin21, pin22, pin23, pin24,
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ GITHUB_URL = "https://github.com/kopp/calliope-mini-stubs"
 
 setup(
     name="calliope-mini-stubs",
-    version="0.1.1",
+    version="0.1.2",
     author="kopp",
     description=("Provide stubs with documentation for "
                  "MicroPython running on Calliope Mini"),
